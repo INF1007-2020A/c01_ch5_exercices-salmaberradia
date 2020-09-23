6b85487d8542f13721286daaae043d716f7d01b8
salmaberradia <71037715+salmaberradia@users.noreply.github.com>
2020-09-22 21:15:45 -0400

    completer l'exercice

--- exercice.py
+++ exercice.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import math
 
 from typing import List
 
@@ -12,28 +12,29 @@ def convert_to_absolute() -> float:
 
 def use_prefixes() -> List[str]:
     prefixes, suffixes = 'JKLMNOPQ', 'ack'
-    
+
     result = []
-    for letter in pefixes:
+    for letter in prefixes:
         result.append(letter + suffixes)
     return result
 
+
 def prime_integer_summation() -> int:
-   primes = []
+    primes = []
 
-   i = 2
-   while lem(primes) < 100:
-       prime = True
+    i = 2
+    while len(primes) < 100:
+        is_prime = True
 
-       for divider in range(2, int(math.sqrt(i)) + 1):
-           if i % divider == 0:
+        for divider in range(2, int(math.sqrt(i)) + 1):
+            if i % divider == 0:
                is_prime = False
 
-         if is_prime:
-             primes.append(i)
+        if is_prime:
+            primes.append(i)
 
         i += 1
-        
+
     return sum(primes)            
 
     
